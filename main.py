2afcf6553fe294f02221d21fc94dc4507eca1130
Palani <palanioffcl@gmail.com>
2023-01-12 20:15:49 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+from src.main import *
+from utils.srv_info import *
+from wsgiref.simple_server import make_server
+
+app = radox()
+
+@app.route("/")
+def index():
+    return "Hello World"
+
+@app.route("/home")
+def home():
+    return "Home Page"
+
+@app.route("/about")
+def about():
+    home()
+    return "A simple WSGI based python backend web framework which can build modern web applications."
+
+if __name__=='__main__':
+    server = make_server('127.0.0.1', 3301, app)
+    info()
+    server.serve_forever()
